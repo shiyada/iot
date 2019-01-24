b28ad0f17ecbb7a50b8ef6f3a2d222638d53c5ed
shiyada <anu.ktprm@gmail.com>
2019-01-24 18:56:22 +0530

    Add files via upload

---
+++ str.ino
@@ -0,0 +1,27 @@
+char RFID_id[100] = "";
+String RFID_id_string;
+    // Store NUID into nuidPICC array
+    for (byte i = 0; i < 4; i++) {
+      nuidPICC[i] = rfid.uid.uidByte[i];
+//      RFID_id[i] = char(rfid.uid.uidByte[i]);
+    }
+
+
+   
+ 
+//Serial.println(myString);
+
+    char str[32] = "";
+    array_to_string(nuidPICC, 4, str);
+    Serial.println(str);
+void array_to_string(byte array[], unsigned int len, char buffer[])
+{
+    for (unsigned int i = 0; i < len; i++)
+    {
+        byte nib1 = (array[i] >> 4) & 0x0F;
+        byte nib2 = (array[i] >> 0) & 0x0F;
+        buffer[i*2+0] = nib1  < 0xA ? '0' + nib1  : 'A' + nib1  - 0xA;
+        buffer[i*2+1] = nib2  < 0xA ? '0' + nib2  : 'A' + nib2  - 0xA;
+    }
+    buffer[len*2] = '\0';
+}
